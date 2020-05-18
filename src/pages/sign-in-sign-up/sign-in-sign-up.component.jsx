561b96157b8b46e92c09e204a909cfb68d8c5cf4
Banjoko Abdulazeez <seunbanjoko2005@yahoo.com>
2020-05-18 12:46:37 +0100

    Completed the implementation of signup with email and password

--- src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
+++ src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import SignIn from '../../components/sign-in/sign-in.component';
+import SignIn from "../../components/sign-in/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
 
-import './sign-in-sign-up.styles.scss';
+import "./sign-in-sign-up.styles.scss";
 
 const SignInSignUp = () => (
-    <div className='sign-in-sign-up'>
-        <div className="sign-in">
-            <SignIn />
-        </div>
-    </div>
-)
+  <div className="sign-in-sign-up">
+    <SignIn />
+    <SignUp />
+  </div>
+);
 
 export default SignInSignUp;
